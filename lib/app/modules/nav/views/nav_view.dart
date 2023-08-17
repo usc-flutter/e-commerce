bb0f89c7720d4cd9e0638aa16d69cfe91d7c242c
usc-flutter <universesoftcare6@gmail.com>
2023-08-17 16:16:09 +0600

    nav problem solve

--- lib/app/modules/nav/views/nav_view.dart
+++ lib/app/modules/nav/views/nav_view.dart
@@ -4,6 +4,7 @@ import 'package:e_commerce/app/modules/homeScreen/views/home_screen_view.dart';
 import 'package:e_commerce/app/modules/profileScreen/views/profile_screen_view.dart';
 import 'package:e_commerce/const/k_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
@@ -19,29 +20,33 @@ class NavView extends GetView<NavController> {
       child: SafeArea(
         child: Scaffold(
           backgroundColor: KColors.scaffold,
-          body: TabBarView(controller: controller.tabController, children: [
-            HomeScreenView(),
-            CartScreenView(),
-            FavouritScreenView(),
-            ProfileScreenView(),
-          ]),
+          body: TabBarView(
+            controller: controller.tabController,
+            physics: const NeverScrollableScrollPhysics(),
+            children: const [
+              HomeScreenView(),
+              CartScreenView(),
+              FavouritScreenView(),
+              ProfileScreenView(),
+            ],
+          ),
           bottomNavigationBar: Container(
             decoration: BoxDecoration(
               color: KColors.white,
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
+                topLeft: Radius.circular(30.h),
+                topRight: Radius.circular(30.h),
               ),
             ),
             child: TabBar(
                 onTap: (value) {
                   controller.change(value);
                 },
-                padding: EdgeInsets.all(10),
+                padding: EdgeInsets.all(8.h),
                 indicatorColor: Colors.transparent,
                 unselectedLabelColor: KColors.black,
                 labelColor: KColors.blue,
-                tabs: [
+                tabs: const [
                   Tab(
                     text: "Home",
                     icon: Icon(
