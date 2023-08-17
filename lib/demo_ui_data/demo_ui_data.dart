c83e0cf86a0014015ecb4ce5d748d99b1732e169
usc-flutter <universesoftcare6@gmail.com>
2023-08-17 16:42:34 +0600

    some demo data add

---
+++ lib/demo_ui_data/demo_ui_data.dart
@@ -0,0 +1,66 @@
+class DemoUiData {
+  String productName;
+  String brandName;
+  String categoris;
+  String imgUrl;
+  double price;
+  DemoUiData({
+    required this.brandName,
+    required this.productName,
+    required this.categoris,
+    required this.imgUrl,
+    required this.price,
+  });
+}
+
+List<DemoUiData> demoUiData = [
+  DemoUiData(
+      brandName: "Apple ltd",
+      productName: "Apple",
+      categoris: "Fruites",
+      imgUrl:
+          "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      price: 50),
+  DemoUiData(
+      brandName: "Apple ltd",
+      productName: "Apple",
+      categoris: "Fruites",
+      imgUrl:
+          "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      price: 50),
+  DemoUiData(
+      brandName: "Apple ltd",
+      productName: "Apple",
+      categoris: "Fruites",
+      imgUrl:
+          "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      price: 50),
+  DemoUiData(
+      brandName: "Apple ltd",
+      productName: "Apple",
+      categoris: "Fruites",
+      imgUrl:
+          "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      price: 50),
+  DemoUiData(
+      brandName: "Apple ltd",
+      productName: "Apple",
+      categoris: "Fruites",
+      imgUrl:
+          "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      price: 50),
+  DemoUiData(
+      brandName: "Apple ltd",
+      productName: "Apple",
+      categoris: "Fruites",
+      imgUrl:
+          "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      price: 50),
+  DemoUiData(
+      brandName: "Apple ltd",
+      productName: "Apple",
+      categoris: "Fruites",
+      imgUrl:
+          "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+      price: 50),
+];
