00e75e053147605aa2cdaefc04d43857c48f2708
usc-flutter <universesoftcare6@gmail.com>
2023-08-18 11:22:30 +0600

    favorite add remove

--- lib/app/modules/favouritScreen/controllers/favourit_screen_controller.dart
+++ lib/app/modules/favouritScreen/controllers/favourit_screen_controller.dart
@@ -1,23 +1,3 @@
 import 'package:get/get.dart';
 
-class FavouritScreenController extends GetxController {
-  //TODO: Implement FavouritScreenController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
-}
+class FavouritScreenController extends GetxController {}
--- lib/app/modules/favouritScreen/views/favourit_screen_view.dart
+++ lib/app/modules/favouritScreen/views/favourit_screen_view.dart
@@ -1,16 +1,19 @@
 import 'package:e_commerce/const/k_colors.dart';
-import 'package:e_commerce/const/k_image_path.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../demo_ui_data/demo_ui_data.dart';
+import '../../homeScreen/controllers/home_screen_controller.dart';
 import '../controllers/favourit_screen_controller.dart';
 
 class FavouritScreenView extends GetView<FavouritScreenController> {
   const FavouritScreenView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var homeScreenController = Get.put(HomeScreenController());
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: Column(
@@ -19,77 +22,95 @@ class FavouritScreenView extends GetView<FavouritScreenController> {
             height: Get.height - 300,
             child: ListView(
               children: [
-                SizedBox(
-                  height: 180.h * 5,
-                  child: ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 5,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10.h),
-                        child: Stack(
-                          children: [
-                            Container(
-                              height: 150.h,
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(5.w)),
-                              child: Row(children: [
-                                10.horizontalSpace,
-                                Image.asset(
-                                  KImagePath.basket,
-                                  height: 100.h,
-                                  width: 100.h,
-                                ),
-                                15.horizontalSpace,
-                                SizedBox(
-                                  width: Get.width / 3,
-                                  child: Column(
+                Obx(
+                  () => SizedBox(
+                    height: 180.h * homeScreenController.favorite.length,
+                    child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: homeScreenController.productList.length,
+                      itemBuilder: (context, index) {
+                        DemoUiData data =
+                            homeScreenController.productList[index];
+                        if (!homeScreenController.favorite
+                            .contains(data.productId)) {
+                          return const SizedBox.shrink();
+                        }
+                        return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10.h),
+                          child: Stack(
+                            children: [
+                              Container(
+                                height: 150.h,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(5.w)),
+                                child: Row(children: [
+                                  10.horizontalSpace,
+                                  Image.network(
+                                    fit: BoxFit.fill,
+                                    data.imgUrl,
+                                    height: 100.h,
+                                    width: 100.h,
+                                  ),
+                                  15.horizontalSpace,
+                                  SizedBox(
+                                    width: Get.width / 3,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "${data.productName} ",
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20.sp),
+                                        ),
+                                        Text(
+                                          "${data.brandName} ",
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16.sp,
+                                              color: Colors.grey),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Column(
                                     mainAxisAlignment: MainAxisAlignment.center,
                                     children: [
-                                      Text(
-                                        "Apple ",
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 20.sp),
-                                      ),
-                                      Text(
-                                        "Apple Name",
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 16.sp,
-                                            color: Colors.grey),
-                                      ),
+                                      Text.rich(TextSpan(
+                                          text: "\$${data.price}",
+                                          style: const TextStyle(
+                                              color: Colors.amber),
+                                          children: [
+                                            const TextSpan(
+                                                text: " kg",
+                                                style: TextStyle(
+                                                    color: Colors.black))
+                                          ])),
                                     ],
                                   ),
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text.rich(TextSpan(
-                                        text: "\$50",
-                                        style: TextStyle(color: Colors.amber),
-                                        children: [
-                                          TextSpan(
-                                              text: " kg",
-                                              style: TextStyle(
-                                                  color: Colors.black))
-                                        ])),
-                                  ],
-                                ),
-                                20.horizontalSpace
-                              ]),
-                            ),
-                            Positioned(
-                                top: 10, right: 10, child: Icon(Icons.cancel))
-                          ],
-                        ),
-                      );
-                    },
+                                  10.horizontalSpace
+                                ]),
+                              ),
+                              Positioned(
+                                  top: 10,
+                                  right: 10,
+                                  child: InkWell(
+                                      onTap: () {
+                                        homeScreenController
+                                            .addOrRemoveFavorit(data.productId);
+                                      },
+                                      child: const Icon(Icons.cancel)))
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
               ],
